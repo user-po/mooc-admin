54d8bf7b2c2d4673680ece60c8161e346f986f27
HZX <zxgldz@gmail.com>
2021-10-19 16:12:38 +0800

    feta:request封装

--- manger-fe/src/main.js
+++ manger-fe/src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import request from './utils/request'
 const app = createApp(App);
+app.config.globalProperties.$request = request;
 app.use(router).use(ElementPlus).mount('#app')
